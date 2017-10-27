b9abf1bc5a6e9c62e8ab206a194490f75938e550
WendyVega <wendyvegaru@gmail.com>
2017-10-27 12:58:34 -0600

    Edit, update y destroy acciones y archivos de vista

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,12 +1,15 @@
 class PicsController < ApplicationController
 	before_action :find_pic, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@pics = Pic.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
-	def new
+	#- necesita view
+	def new 
 		@pic = Pic.new
 	end
 
@@ -18,6 +21,23 @@ class PicsController < ApplicationController
 		else
 			render 'new'
 		end
+	end
+
+	#-necesita view
+	def edit
+	end
+
+	def update
+		if @pic.update(pic_params)
+			redirect_to @pic, notice: "Felicidades! Tu pic fue actualizada"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@pic.destroy
+		redirect_to root_path
 	end
 
 	private
