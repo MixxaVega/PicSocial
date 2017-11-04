d7d40d0f0b27fbc105767d1794054422c547973a
WendyVega <wendyv.vega@gmail.com>
2017-11-04 14:46:36 -0700

    Validaciones de username y password, mensajes en español

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
 	has_attached_file :avatar, style: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
-	validates :username, presence: true
+	validates :username, presence: { message: "No puede estar vacío."}, length: {minimum: 4, maximum: 35, message: "Inválido"}
 	validates :username, uniqueness: true
 end
