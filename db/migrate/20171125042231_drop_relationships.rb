45fe697f0865abda6d954e0fd864f1a94b32d6ef
WendyVega <wendyv.vega@gmail.com>
2017-11-25 12:47:06 -0700

    Modificación de migraciones, limpia de tablas deprecadas

---
+++ db/migrate/20171125042231_drop_relationships.rb
@@ -0,0 +1,6 @@
+class DropRelationships < ActiveRecord::Migration[5.1]
+  def change
+  	drop_table :relationships
+  	drop_table :friendships
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118042708) do
+ActiveRecord::Schema.define(version: 20171125042231) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "body"
@@ -22,14 +22,14 @@ ActiveRecord::Schema.define(version: 20171118042708) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "friendships", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "friendable_type"
-    t.integer "friendable_id"
-    t.integer "friend_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "blocker_id"
-    t.integer "status"
+  create_table "follows", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "following_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+    t.index ["following_id", "follower_id"], name: "index_follows_on_following_id_and_follower_id", unique: true
+    t.index ["following_id"], name: "index_follows_on_following_id"
   end
 
   create_table "pics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
