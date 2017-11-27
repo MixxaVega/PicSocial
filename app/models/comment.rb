0c0f05236d9b8585cb775ff6250f5c7453484378
WendyVega <wendyv.vega@gmail.com>
2017-11-26 18:06:02 -0700

    Se arreglan los issues 5, 6, 7, 8, 9, 10, 11 y 12

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :pic
   belongs_to :user
+
+  validates :body,  length: { maximum: 250, message: "Comentario demasiado largo"}
 end
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -6,6 +6,9 @@ class Pic < ApplicationRecord
 	has_attached_file :image, style: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+	validates :description,  length: { maximum: 300, message: "Descripción demasiado larga"}
+	validates :title,  length: { maximum: 50, message: "Título demasiado largo"}
+
 	def not_vote_by user
 		votes = Vote.where(votable_id: id, voter_id: user.id)
 		votes.delete_all
